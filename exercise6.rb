8eb7cddae1ae9ebdc8b56d394d6a4784e1bcc681
Cobi Moos <cobimoos100@gmail.com>
2015-08-19 14:24:18 -0400

    removed salmon from grocery list and redisplayed groceries

--- exercise6.rb
+++ exercise6.rb
@@ -3,7 +3,7 @@ def printList(list)
   list.each {|item| puts "* #{item}"}
 end
 
-grocery_list = ["bacon", "pork", "ham"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 grocery_list << "rice"
 
 printList(grocery_list)
@@ -13,3 +13,6 @@ grocery_list.include?("bananas") ?  puts("You need to pick up bananas") : puts("
 puts "There are #{grocery_list.length} items in your grocery list"
 
 puts "There second item on your grocery list is #{grocery_list[1]}"
+
+grocery_list.delete("salmon")
+printList(grocery_list)
