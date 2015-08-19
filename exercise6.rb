bdd8b741797ab3d27cc4b9c9985c1451e0d9cc32
Cobi Moos <cobimoos100@gmail.com>
2015-08-19 14:01:57 -0400

    removed an extra printList method line and printList now sorts all items on the grocery list before printing them

--- exercise6.rb
+++ exercise6.rb
@@ -1,9 +1,10 @@
 def printList(list)
+  list.sort!
   list.each {|item| puts "* #{item}"}
 end
 
 grocery_list = ["bacon", "pork", "ham"]
-printList(grocery_list)
+
 
 grocery_list << "rice"
 printList(grocery_list)
