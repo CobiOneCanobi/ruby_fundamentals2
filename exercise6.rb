e6150850852834c8f7f099e39c7a9e356d39498b
Cobi Moos <cobimoos100@gmail.com>
2015-08-19 14:17:56 -0400

    checks if bananas are on your grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -4,12 +4,12 @@ def printList(list)
 end
 
 grocery_list = ["bacon", "pork", "ham"]
-
-
 grocery_list << "rice"
+
 printList(grocery_list)
 
-puts "There are #{grocery_list.length} items in your grocery list"
+grocery_list.include?("bananas") ?  puts("You need to pick up bananas") : puts("You don't need to pick up bananas today")
 
+puts "There are #{grocery_list.length} items in your grocery list"
 
 puts "There second item on your grocery list is #{grocery_list[1]}"
