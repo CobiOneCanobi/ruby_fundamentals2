22143fefcca23eddc82f758585173cbd06c453af
Cobi Moos <cobimoos100@gmail.com>
2015-08-19 15:04:46 -0400

    Exercise 7 completed

--- exercise7.rb
+++ exercise7.rb
@@ -1,3 +1,5 @@
+totalStudents = 0
+
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
@@ -5,13 +7,21 @@ students = {
 }
 def printStudents(x)
   x.each{|cohort, students| puts "#{cohort}: #{students} students"}
+  puts ""
 end
 
 printStudents(students)
 
-students["cohort4"] = 43
+students[:cohort4] = 43
 
 puts students.keys
+puts ""
 
-students.update(students){|cohort, students| students + students * 0.5}
+students.update(students){|cohort, students| students + students * 0.05}
 printStudents(students)
+
+students.delete(:cohort2)
+printStudents(students)
+
+students.each{|cohort, students| totalStudents += students}
+puts "Total amount of students is #{totalStudents}"
