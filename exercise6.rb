2e5f6d8b13af2703f4736df8c7fbafd5fe94ed22
Cobi Moos <cobimoos100@gmail.com>
2015-08-19 13:59:03 -0400

    added a line to print the second item on the grocery list

--- exercise6.rb
+++ exercise6.rb
@@ -9,3 +9,6 @@ grocery_list << "rice"
 printList(grocery_list)
 
 puts "There are #{grocery_list.length} items in your grocery list"
+
+
+puts "There second item on your grocery list is #{grocery_list[1]}"
